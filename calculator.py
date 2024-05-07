534f9b0c6a308381190e1ee5c38e092ee4a043c2
EnesAhmetGul <100159874+EnesAhmetGul@users.noreply.github.com>
2024-05-07 12:53:59 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,25 @@
+# calculator.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
+
+if __name__ == "__main__":
+    num1 = 10
+    num2 = 5
+    
+    print("Addition:", add(num1, num2))
+    print("Subtraction:", subtract(num1, num2))
+    print("Multiplication:", multiply(num1, num2))
+    print("Division:", divide(num1, num2))
